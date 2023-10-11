ef7cb2c6b3473b5f794ddd2d1e3bc114be095c22
ditosoft-ianlabicani <labicaniian@gmail.com>
2023-10-11 17:48:51 +0800

    create table for fetched student

--- index.php
+++ index.php
@@ -14,7 +14,27 @@ require('./inc/dbconn/dbconn.php');
       $stmt->bindParam(':student_id', $search_id, PDO::PARAM_STR);
       $stmt->execute();
       $student = $stmt->fetch(PDO::FETCH_ASSOC);
+      <table border='1'>
+        <tr>
+          <th>Student ID</th>
+          <th>Last Name</th>
+          <th>First Name</th>
+          <th>functions</th>
+        </tr>
         $time_in_button = !get_active_session_by_student_id($_POST['student_id']) ? '<button type="submit" name="time_in">Time In</button>' : '<button disabled type="submit" name="time_in">Time In</button>';
 
         $time_out_button = !get_active_session_by_student_id($_POST['student_id']) ? '<button disabled type="submit" name="time_out">Time Out</button>' : '<button type="submit" name="time_out">Time Out</button>';
 
+        <tr>
+          <td><?= $student['student_id'] ?></td>
+          <td><?= $student['last_name'] ?></td>
+          <td><?= $student['first_name'] ?></td>
+          <td>
+            <form action="session.php" method="post">
+              <input type="hidden" name="student_id" value="<?= $student['student_id'] ?>">
+              <?= $time_in_button ?>
+              <?= $time_out_button ?>
+            </form>
+          </td>
+        </tr>
+      </table>
