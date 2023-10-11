15a2f1ab1025b05170eaa1b803d90295422bc67e
ditosoft-ianlabicani <labicaniian@gmail.com>
2023-10-11 17:47:35 +0800

    fetch a student using search input

--- index.php
+++ index.php
@@ -6,3 +6,11 @@ require('./inc/dbconn/dbconn.php');
     <input type="text" name="student_id" id="student_id" placeholder="e.g., S001">
     <input type="submit" name="search" value="Search">
   </form>
+
+    if (isset($_POST['search'])) {
+      $search_id = $_POST['student_id'];
+      $sql = "SELECT * FROM tbl_students WHERE student_id = :student_id";
+      $stmt = $pdo->prepare($sql);
+      $stmt->bindParam(':student_id', $search_id, PDO::PARAM_STR);
+      $stmt->execute();
+      $student = $stmt->fetch(PDO::FETCH_ASSOC);
