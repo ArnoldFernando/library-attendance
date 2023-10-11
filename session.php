689c5c09ae0e119c40238dbd09d5b68f9a3e3985
ditosoft-ianlabicani <labicaniian@gmail.com>
2023-10-11 17:52:40 +0800

    add session.php

--- session.php
+++ session.php
@@ -1,3 +1,8 @@
+<?php
+require('./inc/dbconn/dbconn.php');
+
+date_default_timezone_set('Asia/Manila');
+
 function open_session($student_id)
 {
   require('./inc/dbconn/dbconn.php');
@@ -32,6 +37,8 @@ function close_session($student_id)
     $stmt->bindParam(':session_id', $session['session_id'], PDO::PARAM_STR);
     $stmt->execute();
 
+    // setting session invalid
+
     $sql_close_validity = "UPDATE tbl_sessions SET validity = false WHERE session_id = :session_id";
     $stmt = $pdo->prepare($sql_close_validity);
     $stmt->bindParam(':session_id', $session['session_id'], PDO::PARAM_STR);
@@ -40,3 +47,15 @@ function close_session($student_id)
     $sql_calculate = "UPDATE tbl_sessions SET total_minutes = TIME_TO_SEC(TIMEDIFF(time_out, time_in)) / 60;";
     $stmt = $pdo->prepare($sql_calculate);
     $stmt->execute();
+  }
+}
+
+if (isset($_POST['time_in'])) {
+  open_session($_POST['student_id']);
+  header('location: index.php');
+}
+
+if (isset($_POST['time_out'])) {
+  close_session($_POST['student_id']);
+  header('location: index.php');
+}
