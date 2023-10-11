4276243296a342af29c32d83e3dafd9e71dfa904
ditosoft-ianlabicani <labicaniian@gmail.com>
2023-10-11 17:49:46 +0800

    create index

--- index.php
+++ index.php
@@ -10,12 +10,28 @@ function get_active_session_by_student_id($student_id)
   $stmt->execute();
   return $stmt->fetch(PDO::FETCH_ASSOC) ?? null;
 }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Library Attendance</title>
+</head>
+
+<body>
+  <h1>Welcome</h1>
   <form method="post">
     <label for="student_id">Enter Student ID:</label>
     <input type="text" name="student_id" id="student_id" placeholder="e.g., S001">
     <input type="submit" name="search" value="Search">
   </form>
 
+  <?php
+  try {
     if (isset($_POST['search'])) {
       $search_id = $_POST['student_id'];
       $sql = "SELECT * FROM tbl_students WHERE student_id = :student_id";
@@ -23,6 +39,7 @@ function get_active_session_by_student_id($student_id)
       $stmt->bindParam(':student_id', $search_id, PDO::PARAM_STR);
       $stmt->execute();
       $student = $stmt->fetch(PDO::FETCH_ASSOC);
+  ?>
       <table border='1'>
         <tr>
           <th>Student ID</th>
@@ -30,10 +47,17 @@ function get_active_session_by_student_id($student_id)
           <th>First Name</th>
           <th>functions</th>
         </tr>
+
+        <?php
+
+        // echo "<hr>";
+        // echo var_dump(get_active_session_by_student_id($_POST['student_id']));
+        // echo "<hr>";
         $time_in_button = !get_active_session_by_student_id($_POST['student_id']) ? '<button type="submit" name="time_in">Time In</button>' : '<button disabled type="submit" name="time_in">Time In</button>';
 
         $time_out_button = !get_active_session_by_student_id($_POST['student_id']) ? '<button disabled type="submit" name="time_out">Time Out</button>' : '<button type="submit" name="time_out">Time Out</button>';
 
+        ?>
         <tr>
           <td><?= $student['student_id'] ?></td>
           <td><?= $student['last_name'] ?></td>
@@ -47,3 +71,18 @@ function get_active_session_by_student_id($student_id)
           </td>
         </tr>
       </table>
+  <?php
+    }
+  } catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+  }
+  ?>
+</body>
+
+</html>
+
+<!-- 
+
+  when I click "time in" button, I want to create a session with that student_id
+  
+-->
