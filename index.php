3464e1f3b6305750afb5de02471b2e74038b3b40
ditosoft-ianlabicani <labicaniian@gmail.com>
2023-10-11 17:46:58 +0800

    create search form

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php
 require('./inc/dbconn/dbconn.php');
+
+  <form method="post">
+    <label for="student_id">Enter Student ID:</label>
+    <input type="text" name="student_id" id="student_id" placeholder="e.g., S001">
+    <input type="submit" name="search" value="Search">
+  </form>
