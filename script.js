d6e19e3aaecfd4683f4bcb0843438d2b4f6975d8
ditosoft-ianlabicani <labicaniian@gmail.com>
2023-10-14 10:44:59 +0800

    traverse the elements where class is session-sec and update every sec

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
+let sessionSecElements = document.querySelectorAll(".session-sec");
 function updateSeconds(element) {
 	let initialSeconds = parseInt(element.textContent, 10);
 	initialSeconds++;
 	element.textContent = initialSeconds;
 }
+
+sessionSecElements.forEach(function (element) {
+	setInterval(function () {
+		updateSeconds(element);
+	}, 1000);
+});
