8ab713ccf96468088646250c7c13f1348921e857
ditosoft-ianlabicani <labicaniian@gmail.com>
2023-10-14 10:41:43 +0800

    refactor by making a function getAllActiveSessions from tbl_sessions.queries.php

--- index.php
+++ index.php
@@ -25,8 +25,6 @@ startSystem();
     <input type="text" name="student_id" id="student_id" placeholder="e.g., S001">
     <input type="submit" name="search" value="Search" required>
   </form>
-  <br><br><br>
-  <hr>
 
   <?php
   try {
@@ -38,65 +36,49 @@ startSystem();
         <h1>no student found!</h1>
       <?php
       } else {
-  ?>
-      <table border='1'>
-        <tr>
-          <th>Student ID</th>
-          <th>Last Name</th>
-          <th>First Name</th>
-          <th>functions</th>
-        </tr>
+      ?>
+        <table border='1'>
+          <tr>
+            <th>Student ID</th>
+            <th>Last Name</th>
+            <th>First Name</th>
+            <th>functions</th>
+          </tr>
 
-        <?php
-        $time_in_button = !get_active_session_by_student_id($_POST['student_id']) ? '<button type="submit" name="time_in">Time In</button>' : '<button disabled type="submit" name="time_in">Time In</button>';
-
-        $time_out_button = !get_active_session_by_student_id($_POST['student_id']) ? '<button disabled type="submit" name="time_out">Time Out</button>' : '<button type="submit" name="time_out">Time Out</button>';
-
-        ?>
-        <tr>
-          <td><?= $student['student_id'] ?></td>
-          <td><?= $student['last_name'] ?></td>
-          <td><?= $student['first_name'] ?></td>
-          <td>
-            <form action="session.php" method="post">
-              <input type="hidden" name="student_id" value="<?= $student['student_id'] ?>">
+          <tr>
+            <td><?= $student['student_id'] ?></td>
+            <td><?= $student['last_name'] ?></td>
+            <td><?= $student['first_name'] ?></td>
+            <td>
+              <form action="session.php" method="post">
+                <input type="hidden" name="student_id" value="<?= $student['student_id'] ?>">
                 <button <?= (hasActiveSession($search_id) ? "disabled" : "") ?> type="submit" name="time_in">Time In</button>
                 <button <?= (!hasActiveSession($search_id) ? "disabled" : "") ?> type="submit" name="time_out">Time Out</button>
-            </form>
-          </td>
-        </tr>
-      </table>
-      <br><br>
-      <hr><br><br>
-    <?php
+              </form>
+            </td>
+          </tr>
+        </table>
+
+        <h1>active sessions</h1>
+  <?php
+      }
     }
 
-
-    $sql = "SELECT * FROM tbl_students LIMIT 10;";
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute();
-
-    while ($students = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    ?>
-      <table border='1'>
-        <tr>
-          <th>Student ID</th>
-          <th>Last Name</th>
-          <th>First Name</th>
-        </tr>
-        <tr>
-          <td><?= $students['student_id'] ?></td>
-          <td><?= $students['last_name'] ?></td>
-          <td><?= $students['first_name'] ?></td>
-        </tr>
-      </table>
-
-  <?php
+    $active_sessions = getAllActiveSessions();
+    // print_r($active_sessions);
+    foreach ($active_sessions as $active_session) {
+      // Process each session
+      echo "<hr>Session ID: " . $active_session['session_id'] . "<br>";
+      echo "Student name: " . $active_session['first_name'] . $active_session['last_name'] . "<br>";
+      echo "Student session duration in seconds: <strong class='session-sec'>" .  $active_session['session_seconds'] . "</strong> seconds<hr>";
     }
   } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
   }
   ?>
+
+
+  <script src="./script.js" defer></script>
 </body>
 
 </html>
